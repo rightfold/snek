d8314beba6b77289b52f6325e1a29e10d673d64b
rightfold <rightfold@gmail.com>
2016-05-07 18:42:37 +0200

    Implement parsing names and calls

---
+++ src/SNEK/Parse.hs
@@ -0,0 +1,30 @@
+module SNEK.Parse
+( ParseError(..)
+
+, parseTE
+, parseVE
+) where
+
+import SNEK.AST (TE(..), VE(..))
+import SNEK.Data (Datum(..))
+
+data ParseError
+  = CallWithoutCallee
+  deriving (Eq, Show)
+
+parseTE :: Datum -> Either ParseError (TE ())
+parseTE (Symbol name) = return $ NameTE () name
+parseTE _ = error "not yet implemented"
+
+parseVE :: Datum -> Either ParseError (VE () () ())
+parseVE (Symbol name)    = return $ NameVE () name
+parseVE (List  [])       = Left CallWithoutCallee
+parseVE (Array [])       = Left CallWithoutCallee
+parseVE (List  (f : as)) = do
+  f' <- parseVE f
+  as' <- mapM parseVE as
+  return $ foldl ValueApplyVE f' as'
+parseVE (Array (f : as)) = do
+  f' <- parseVE f
+  as' <- mapM parseTE as
+  return $ foldl TypeApplyVE f' as'
---
+++ test/SNEK/ParseSpec.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE LambdaCase #-}
+module SNEK.ParseSpec
+( spec
+) where
+
+import SNEK.AST (TE(..), VE(..))
+import SNEK.Data (Datum(..))
+import SNEK.Parse
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "parseTE NameVE" $ do
+    it "ok" $ do
+      parseTE (Symbol "x") `shouldSatisfy` \case
+        Right (NameTE () "x") -> True
+        _ -> False
+  describe "parseVE NameVE" $ do
+    it "ok" $ do
+      parseVE (Symbol "x") `shouldSatisfy` \case
+        Right (NameVE () "x") -> True
+        _ -> False
+  describe "parseVE ValueApplyVE" $ do
+    it "ok" $ do
+      parseVE (List []) `shouldSatisfy` \case
+        Left CallWithoutCallee -> True
+        _ -> False
+      parseVE (List [Symbol "x"]) `shouldSatisfy` \case
+        Right (NameVE () "x") -> True
+        _ -> False
+      parseVE (List [Symbol "x", Symbol "y"]) `shouldSatisfy` \case
+        Right (ValueApplyVE (NameVE () "x") (NameVE () "y")) -> True
+        _ -> False
+      parseVE (List [Symbol "x", Symbol "y", Symbol "z"]) `shouldSatisfy` \case
+        Right (ValueApplyVE (ValueApplyVE (NameVE () "x") (NameVE () "y"))
+                            (NameVE () "z")) -> True
+        _ -> False
+  describe "parseVE ValueApplyTE" $ do
+    it "ok" $ do
+      parseVE (Array []) `shouldSatisfy` \case
+        Left CallWithoutCallee -> True
+        _ -> False
+      parseVE (Array [Symbol "x"]) `shouldSatisfy` \case
+        Right (NameVE () "x") -> True
+        _ -> False
+      parseVE (Array [Symbol "x", Symbol "y"]) `shouldSatisfy` \case
+        Right (TypeApplyVE (NameVE () "x") (NameTE () "y")) -> True
+        _ -> False
+      parseVE (Array [Symbol "x", Symbol "y", Symbol "z"]) `shouldSatisfy` \case
+        Right (TypeApplyVE (TypeApplyVE (NameVE () "x") (NameTE () "y"))
+                           (NameTE () "z")) -> True
+        _ -> False
