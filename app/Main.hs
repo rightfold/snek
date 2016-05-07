b24a2edeeac920942632d1c1f5106bb46e70a581
rightfold <rightfold@gmail.com>
2016-05-07 19:22:39 +0200

    Parse fn special form

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,16 @@
 module Main where
 
+import Control.Lens ((%~))
 import Control.Monad.Reader (runReader)
 import Control.Monad.Trans.Either (runEitherT)
-import SNEK.Check (checkVE, emptyE)
+import Data.Function ((&))
+import SNEK.Check (checkVE, emptyE, eTSs, eVSs)
 import SNEK.Parse (parseVE)
 import SNEK.Read (readData)
+import SNEK.Symbol (TS(..), VS(..))
+import SNEK.Type ((~->~), T(..))
+
+import qualified Data.Map as Map
 
 main :: IO ()
 main = interact $ \text ->
@@ -12,8 +18,12 @@ main = interact $ \text ->
     Just data_ ->
       case mapM parseVE data_ of
         Right ast ->
-          case mapM (\e -> runReader (runEitherT (checkVE e)) emptyE) ast of
+          case mapM (\e -> runReader (runEitherT (checkVE e)) env) ast of
             Right tast -> show tast ++ "\n"
             Left  err  -> show err ++ "\n"
         Left  err -> show err ++ "\n"
     Nothing -> show "read error\n"
+
+  where env = emptyE
+              & eVSs %~ Map.insert "not" (VS (BoolT ~->~ BoolT))
+              & eTSs %~ Map.insert "bool" (TS BoolT)
--- src/SNEK/Parse.hs
+++ src/SNEK/Parse.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module SNEK.Parse
 ( ParseError(..)
 
@@ -5,12 +7,15 @@ module SNEK.Parse
 , parseVE
 ) where
 
+import Control.Monad (forM, when)
+import Data.List.Split (chunksOf)
 import SNEK.AST (TE(..), VE(..))
 import SNEK.Data (Datum(..))
 
 data ParseError
   = CallWithoutCallee
   | CallWithoutArgument
+  | IllFormedSpecialForm
   deriving (Eq, Show)
 
 parseTE :: Datum -> Either ParseError (TE ())
@@ -19,15 +24,38 @@ parseTE _ = error "not yet implemented"
 
 parseVE :: Datum -> Either ParseError (VE () () ())
 parseVE (Symbol name)    = return $ NameVE () name
-parseVE (List  [])       = Left CallWithoutCallee
-parseVE (Array [])       = Left CallWithoutCallee
-parseVE (List  [_])      = Left CallWithoutArgument
-parseVE (Array [_])      = Left CallWithoutArgument
+parseVE (List  [])       = throwError CallWithoutCallee
+parseVE (Array [])       = throwError CallWithoutCallee
+parseVE (List  [_])      = throwError CallWithoutArgument
+parseVE (Array [_])      = throwError CallWithoutArgument
 parseVE (List  (f : as)) = do
-  f' <- parseVE f
-  as' <- mapM parseVE as
-  return $ foldl ValueApplyVE f' as'
+  special <- parseVESpecial f as
+  case special of
+    Just ve -> return ve
+    Nothing -> do
+      f' <- parseVE f
+      as' <- mapM parseVE as
+      return $ foldl ValueApplyVE f' as'
 parseVE (Array (f : as)) = do
   f' <- parseVE f
   as' <- mapM parseTE as
   return $ foldl TypeApplyVE f' as'
+
+parseVESpecial :: Datum -> [Datum] -> Either ParseError (Maybe (VE () () ()))
+parseVESpecial (Symbol "fn") as = Just <$> go
+  where go = case as of
+          [List ps, b] -> do
+            ps' <- parseParams ps
+            b' <- parseVE b
+            return $ foldr (\(p, t) -> ValueLambdaVE p t) b' ps'
+          _ -> throwError IllFormedSpecialForm
+        parseParams ps = do
+          let pairs = (chunksOf 2 ps)
+          when (null pairs) (throwError IllFormedSpecialForm)
+          forM pairs $ \case
+            [Symbol n, t] -> (n,) <$> parseTE t
+            _ -> throwError IllFormedSpecialForm
+parseVESpecial _ _ = return Nothing
+
+throwError :: a -> Either a b
+throwError = Left
