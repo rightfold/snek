2036047d843f1a1b41eff9c6b4bb0f6ada670d2d
rightfold <rightfold@gmail.com>
2016-05-07 18:57:20 +0200

    Implement playground-style main action

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import Control.Monad.Reader (runReader)
+import Control.Monad.Trans.Either (runEitherT)
+import SNEK.Check (checkVE, emptyE)
+import SNEK.Parse (parseVE)
+import SNEK.Read (readData)
 
 main :: IO ()
-main = someFunc
+main = interact $ \text ->
+  case readData text of
+    Just data_ ->
+      case mapM parseVE data_ of
+        Right ast ->
+          case mapM (\e -> runReader (runEitherT (checkVE e)) emptyE) ast of
+            Right tast -> show tast ++ "\n"
+            Left  err  -> show err ++ "\n"
+        Left  err -> show err ++ "\n"
+    Nothing -> show "read error\n"
